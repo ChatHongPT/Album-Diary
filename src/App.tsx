5bcb337f9ce9ca2153b566ca56f692711192d6c4
suk924600 <suk924600@naver.com>
2025-02-09 15:54:37 +0900

    feat: pages(bookmark)추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import MainPage from '@/pages/index'
+import AboutPage from '@/pages/about'
 function App() {
   return (
-    <div>App 컴포넌트 화면입니다.</div>
+    <BrowserRouter>
+        <Routes>
+          <Route index path = "/" element={<MainPage />}></Route>
+          <Route path = "/about" element={<AboutPage />}></Route>
+          <Route path = "/about/:id" element={<AboutPage />}></Route>
+
+        </Routes>
+    </BrowserRouter>
   )
 }
 
